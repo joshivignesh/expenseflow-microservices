88452570d517bd58b514fd400d7d640944911897
Vignesh <joshi.vignesh@gmail.com>
2026-02-27 13:47:39 +0530

    feat(identity): add FluentValidation rules for RegisterUser command

---
+++ src/services/ExpenseFlow.Identity/ExpenseFlow.Identity.Application/Commands/RegisterUser/RegisterUserCommandValidator.cs
@@ -0,0 +1,19 @@
+using FluentValidation;
+
+namespace ExpenseFlow.Identity.Application.Commands.RegisterUser;
+
+public sealed class RegisterUserCommandValidator : AbstractValidator<RegisterUserCommand>
+{
+    public RegisterUserCommandValidator()
+    {
+        RuleFor(x => x.FirstName).NotEmpty().MaximumLength(100);
+        RuleFor(x => x.LastName).NotEmpty().MaximumLength(100);
+        RuleFor(x => x.Email).NotEmpty().EmailAddress();
+        RuleFor(x => x.Password)
+            .NotEmpty()
+            .MinimumLength(8)
+            .Matches("[A-Z]").WithMessage("Password must contain at least one uppercase letter.")
+            .Matches("[0-9]").WithMessage("Password must contain at least one digit.")
+            .Matches("[^a-zA-Z0-9]").WithMessage("Password must contain at least one special character.");
+    }
+}
